3f897e29f931bd2aadd7011fb428e8c3456ad556
ouyangxizhu <1372586016@qq.com>
2019-06-01 13:25:40 +0800

    array and linkedlist all

---
+++ src/_zerosix/OneFourTwo4.java
@@ -0,0 +1,45 @@
+package _zerosix;
+
+import util.ListNode;
+
+import java.util.HashSet;
+import java.util.ServiceConfigurationError;
+
+/**
+ * https://leetcode-cn.com/problems/linked-list-cycle/
+ */
+public class OneFourOne3 {
+    /**
+     * 快慢指针 空间复杂度O(1),时间复杂度O(N)
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        if(head==null||head.next==null)return false;
+        ListNode fast=head,slow=head;
+        while(fast.next!=null&&fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * set判断重复。空间复杂度O(N),时间复杂度O(N)
+     * @param head
+     * @return
+     */
+    public boolean hasCycle1(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (head != null){
+            if (set.contains(head)) return true;
+            else
+                set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
---
+++ src/_zerosix/TwoFive5.java
@@ -0,0 +1,4 @@
+package _zerosix;
+
+public class TwoFive5 {
+}
