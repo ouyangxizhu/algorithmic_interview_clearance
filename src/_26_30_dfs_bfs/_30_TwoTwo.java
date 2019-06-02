711b8679f2d287ac35d2c6104d1f2146f78c8789
ouyangxizhu <1372586016@qq.com>
2019-06-02 16:35:25 +0800

    all dfs and bfs

--- src/_26_30_dfs_bfs/_30_TwoTwo.java
+++ src/_26_30_dfs_bfs/_30_TwoTwo.java
@@ -1,4 +1,33 @@
 package _26_30_dfs_bfs;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/generate-parentheses/
+ */
 public class _30_TwoTwo {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList();
+        generateOneByOne("", list, n, n);
+        return list;
+    }
+
+    public static void generateOneByOne(String sublist, List<String> list, int left, int right) {
+        if (left > right) {
+            return;
+        }
+
+        if (left > 0) {
+            generateOneByOne(sublist + "(", list, left - 1, right);
+        }
+
+        if (right > 0) {
+            generateOneByOne(sublist + ")", list, left, right - 1);
+        }
+
+        if (left == 0 && right == 0) {
+            list.add(sublist);
+        }
+    }
 }
