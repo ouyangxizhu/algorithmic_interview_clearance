aa90bd99ef43dbe8b6ebd22d5e31e4a111b55561
ouyangxizhu <1372586016@qq.com>
2019-06-02 14:37:00 +0800

    recursion and divide all

--- src/_20_23_Recursion_Divide/_23_OneSixNine.java
+++ src/_20_23_Recursion_Divide/_23_OneSixNine.java
@@ -1,6 +1,13 @@
 package _20_23_Recursion_Divide;
 
 public class _23_OneSixNine {
+    /**
+     * 1. 用hashmap存储数字和次数，只有遍历，求出最大的
+     * 2. 排序之后直接去中间的数
+     * 3. 计数
+     * @param nums
+     * @return
+     */
     public int majorityElement(int[] nums) {
         if (nums.length==1) {
             return nums[0];
